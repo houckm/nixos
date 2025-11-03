7d9b53676634ae7f5233fe69584b452cefd7d701
Hunter Houck <hhouck.linux@gmail.com>
2025-11-03 11:31:56 -0600

    disk partitioning for homeserver

---
+++ hosts/homeserver/disko.nix
@@ -0,0 +1,33 @@
+{ lib, ... }:
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/sda";  # Change to your actual disk
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
