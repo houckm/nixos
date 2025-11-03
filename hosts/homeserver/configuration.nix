2461dd61badb4feaa1eb127f7f284bddb7f0d1e4
Hunter Houck <hhouck.linux@gmail.com>
2025-11-02 23:04:39 -0600

    homeserver configuration.nix

---
+++ hosts/homeserver/configuration.nix
@@ -0,0 +1,65 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "homeserver";
+  networking.networkmanager.enable = true;
+
+  # Time zone
+  time.timeZone = "America/Chicago";
+
+  # Locale
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # SSH - critical for remote access
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+
+  # User account
+  users.users.hunter = {
+    isNormalUser = true;
+    description = "Hunter Houck";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    shell = pkgs.bash;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIClIg1RpM0C1yrFeP2avju5rYMmmYIdRkE0WwRw3Rah hhouck.linux@gmail.com"
+    ];
+  };
+
+
+  # Firewall
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 22 ];  # SSH
+  };
+
+  # Docker for containers
+  virtualisation.docker.enable = true;
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    htop
+    wget
+    curl
+    tmux
+  ];
+
+  # Flakes support
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "25.05";
+}
