fdadd4581b6b4f46b7345dc5d3fa9a268bf14a54
Hunter Houck <hhouck.linux@gmail.com>
2025-11-02 22:52:43 -0600

    Revert "homeserver configuration.nix"
    This reverts commit ed349bf0bf309cd69523c142627ea1ffc184a461.

--- flake.nix
+++ flake.nix
@@ -12,11 +12,49 @@ outputs = { nixpkgs, home-manager, claude-code, ... }: {
       }
     ];
   };
+<<<<<<< HEAD
   
   nixosConfigurations.homeserver = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
       ./hosts/homeserver/configuration.nix
     ];
+||||||| ed349bf (homeserver configuration.nix)
+
+  outputs = { nixpkgs, home-manager, claude-code, ... }: {
+    nixosConfigurations.spaceship = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/spaceship/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.hunter = import ./home/home.nix;
+          nixpkgs.overlays = [ claude-code.overlays.default ];
+        }
+      ];
+    };
+    homeserver = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/homeserver/configuration.nix
+=======
+
+  outputs = { nixpkgs, home-manager, claude-code, ... }: {
+    nixosConfigurations.spaceship = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/spaceship/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.hunter = import ./home/home.nix;
+          nixpkgs.overlays = [ claude-code.overlays.default ];
+        }
+      ];
+    };
+>>>>>>> parent of ed349bf (homeserver configuration.nix)
   };
 };
